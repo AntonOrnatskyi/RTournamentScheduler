ccff94605647237fc690e3a6d9f11ebc3e1e5639
Anton Ornatskyi <heilig.thesaint@gmail.com>
2015-09-18 00:13:29 +0300

    Beta candidate
    Fixed team blocking at last tables

--- Tournament.R
+++ Tournament.R
@@ -1,5 +1,3 @@
-set.seed(15)
-
 players <- data.frame(Num = c(1:100), TeamNum = rep(c(1:25),4))
 plHistory<-data.frame(PlayerNum = integer(), Played = integer(), Repeats = integer())
 placedPlayers <- data.frame(Num = integer(), TeamNum = integer())
@@ -15,21 +13,30 @@ for (rounds in 1:dim(tables)[3]) {
     seated<-integer()
     avPlayers<-subset(players, !(players$Num %in% placedPlayers$Num))
     for (places in 1:dim(tables)[1]) {
-      if (nrow(avPlayers) == 10) {
-        
+      teamRest<-aggregate(data = avPlayers, .~TeamNum, FUN = length)
+      if (max(teamRest$Num) == 10 - nrow(placedPlayers) %/% 10) {
+          critTeams<-subset(teamRest, teamRest$Num==max(teamRest$Num))
+          critTeamPlayers<-subset(avPlayers,avPlayers$TeamNum %in% critTeams$TeamNum)
+          if (nrow(critTeamPlayers) > 1) {
+            setPlayer<-critTeamPlayers[sample(nrow(critTeamPlayers),1),]
+          }
+          else {
+            setPlayer<-critTeamPlayers[1,]
+          }
       }
-      if (nrow(avPlayers) > 1) {
-        setPlayer<-avPlayers[sample(nrow(avPlayers),1),]
-      }
-      else { 
-        setPlayer<-avPlayers[1,]
-        browser()
+      else {
+        if (nrow(avPlayers) > 1) {
+          setPlayer<-avPlayers[sample(nrow(avPlayers),1),]
+        }
+        else { 
+          setPlayer<-avPlayers[1,]
+          browser()
+        }
       }
       placedPlayers<-rbind(placedPlayers,setPlayer)
       avPlayers<-subset(avPlayers, avPlayers$TeamNum != setPlayer$TeamNum)
-      played<-subset(plHistory, plHistory$PlayerNum ==  setPlayer$Num && plHistory$Repeats > 2)
+      played<-subset(plHistory, plHistory$PlayerNum ==  setPlayer$Num && plHistory$Repeats > 1)
       avPlayers<-subset(avPlayers, !(avPlayers$Num %in% played$Played))
-      if (nrow(avPlayers)==0) browser()
       tables[places, tableNum, rounds]<-setPlayer$Num
       seated<-c(seated,rep(setPlayer$Num,10))
     }
